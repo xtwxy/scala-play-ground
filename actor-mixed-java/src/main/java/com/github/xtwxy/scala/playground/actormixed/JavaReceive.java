64ce90b6944f7667b6534ff88295555f1c300344
Wangxy <xtwxy@hotmail.com>
2018-03-01 15:43:05 +0800

    renamed actor to self to remove confusion.

--- actor-mixed-java/src/main/java/com/github/xtwxy/scala/playground/actormixed/JavaReceive.java
+++ actor-mixed-java/src/main/java/com/github/xtwxy/scala/playground/actormixed/JavaReceive.java
@@ -7,23 +7,23 @@ import akka.japi.pf.ReceiveBuilder;
 import static java.lang.System.out;
 
 public class JavaReceive {
-    public static AbstractActor.Receive create(Actor actor) {
+    public static AbstractActor.Receive create(Actor self) {
         return ReceiveBuilder.create()
                 .matchEquals("hello", s -> {
                     out.println("initial => hello.");
-                    actor.context().become(hello(actor).onMessage(), true);
-                    actor.sender().tell("world", actor.self());
+                    self.context().become(hello(self).onMessage(), true);
+                    self.sender().tell("world", self.self());
                 })
                 .matchAny(o -> out.println(o.toString()))
                 .build();
     }
 
-    public static AbstractActor.Receive hello(Actor actor) {
+    public static AbstractActor.Receive hello(Actor self) {
         return ReceiveBuilder.create()
                 .matchAny(o -> {
                     out.println("initial <= hello.");
-                    actor.context().unbecome();
-                    actor.sender().tell("hello", actor.self());
+                    self.context().unbecome();
+                    self.sender().tell("hello", self.self());
                 })
                 .build();
     }

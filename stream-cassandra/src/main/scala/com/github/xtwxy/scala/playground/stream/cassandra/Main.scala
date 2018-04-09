eac042668ab52e1e562e7e6792ef6dab939d836a
Wangxy <xtwxy@hotmail.com>
2018-04-09 17:56:02 +0800

    add cassandra codec.

--- stream-cassandra/src/main/scala/com/github/xtwxy/scala/playground/stream/cassandra/Main.scala
+++ stream-cassandra/src/main/scala/com/github/xtwxy/scala/playground/stream/cassandra/Main.scala
@@ -6,6 +6,8 @@ import akka.stream._
 import akka.stream.alpakka.cassandra.scaladsl._
 import akka.stream.scaladsl._
 import com.datastax.driver.core.{Cluster, PreparedStatement}
+import com.github.xtwxy.`scala`.playground.stream.cassandra.SignalType._
+import com.google.protobuf.timestamp.Timestamp
 
 object Main extends App {
   implicit val system = ActorSystem("Cassandra")
@@ -18,17 +20,27 @@ object Main extends App {
     .withPort(9042)
     .build()
 
+  cluster
+    .getConfiguration()
+    .getCodecRegistry()
+    .register(SignalValueCodec())
+
   implicit val session = cluster.connect()
 
-  val source: Source[Integer, SourceQueueWithComplete[Integer]] = Source.queue[Integer](1000, OverflowStrategy.dropNew)
+  val source: Source[SignalValueLog, SourceQueueWithComplete[SignalValueLog]] = Source.queue[SignalValueLog](1000, OverflowStrategy.dropNew)
 
   val keyspaceName = "stream"
-  val preparedStatement = session.prepare(s"INSERT INTO $keyspaceName.test(id) VALUES (?)")
-  val statementBinder = (myInteger: Integer, statement: PreparedStatement) => statement.bind(myInteger)
-  val sink = CassandraSink[Integer](2, preparedStatement, statementBinder)
+  val preparedStatement = session.prepare(s"INSERT INTO $keyspaceName.signal_log(id, ts, value) VALUES (?, ?, ?)")
+  val statementBinder = (log: SignalValueLog, statement: PreparedStatement) => statement.bind(log.id, log.ts, log.value)
+  val sink = CassandraSink[SignalValueLog](2, preparedStatement, statementBinder)
   val (queue, result) = source.toMat(sink)(Keep.both).run()
 
-  (0 until 10).foreach(x => queue.offer(x))
+  Seq(
+    SignalValueLog("1", Some(Timestamp(System.currentTimeMillis()/1000, 0)), Some(SignalValue(AI, 12.3, false, ""))),
+    SignalValueLog("2", Some(Timestamp(System.currentTimeMillis()/1000, 0)), Some(SignalValue(DI, 0, true, ""))),
+    SignalValueLog("3", Some(Timestamp(System.currentTimeMillis()/1000, 0)), Some(SignalValue(SI, 0, false, "hello"))),
+  ).foreach(x => queue.offer(x))
+
   queue.complete()
 
   result.onComplete(_ ⇒ {
---
+++ stream-cassandra/src/main/scala/com/github/xtwxy/scala/playground/stream/cassandra/SignalValueCodec.scala
@@ -0,0 +1,28 @@
+package com.github.xtwxy.scala.playground.stream.cassandra
+
+import java.nio.ByteBuffer
+
+import com.datastax.driver.core.{DataType, ProtocolVersion, TypeCodec}
+import com.trueaccord.scalapb.TextFormat
+
+object SignalValueCodec {
+  def apply(): SignalValueCodec = new SignalValueCodec()
+}
+
+class SignalValueCodec extends TypeCodec[SignalValue](DataType.blob(), classOf[SignalValue]) {
+  override def format(value: SignalValue): String = TextFormat.printToString(value)
+
+  override def parse(value: String): SignalValue = TextFormat.fromAscii[SignalValue](SignalValue, value).right.get
+
+  override def serialize(value: SignalValue, protocolVersion: ProtocolVersion): ByteBuffer = {
+    val bytes = value.toByteArray
+    val buffer = ByteBuffer.allocate(bytes.length)
+    buffer.put(bytes)
+    buffer.flip()
+    buffer
+  }
+
+  override def deserialize(bytes: ByteBuffer, protocolVersion: ProtocolVersion): SignalValue = {
+    SignalValue.parseFrom(bytes.array())
+  }
+}
